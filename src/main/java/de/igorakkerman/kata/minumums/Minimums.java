71c0aea642f514375b1989ff174fa12edcd9f3ba
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-05-02 15:44:58 +0200

    Streams joined using flatMap

--- src/main/java/de/igorakkerman/kata/minumums/Minimums.java
+++ src/main/java/de/igorakkerman/kata/minumums/Minimums.java
@@ -2,11 +2,12 @@ package de.igorakkerman.kata.minumums;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
 
 public class Minimums {
     public static List<Integer> minimums(List<Integer> input, int count) {
@@ -42,6 +43,6 @@ public class Minimums {
         List<Integer> sortedLow = sort(low, count);
         List<Integer> sortedHigh = sortedLow.size() < count - 1 ? sort(high, count - sortedLow.size() - 1) : high;
 
-        return Stream.concat(Stream.concat(sortedLow.stream(), Stream.of(pivot)), sortedHigh.stream()).limit(count).collect(toList());
+        return Stream.of(sortedLow.stream(), Stream.of(pivot), sortedHigh.stream()).flatMap(Function.identity()).limit(count).collect(toList());
     }
 }
