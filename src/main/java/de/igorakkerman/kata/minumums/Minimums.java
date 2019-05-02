1f24aa7f53f478a8e66d18c8556c540b8ca5a999
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-05-02 14:34:24 +0200

    count > input.size() handled

--- src/main/java/de/igorakkerman/kata/minumums/Minimums.java
+++ src/main/java/de/igorakkerman/kata/minumums/Minimums.java
@@ -6,9 +6,12 @@ import java.util.List;
 import static java.util.Collections.emptyList;
 
 public class Minimums {
-    public static List<Integer> minimums(List<Integer> items, int count) {
+    public static List<Integer> minimums(List<Integer> input, int count) {
         if (count < 0)
             throw new IllegalArgumentException("count == " + count + " < 0");
+
+        if (count > input.size())
+            throw new IllegalArgumentException("count == " + count + " > " + input.size() + " == input.size()");
 
         return emptyList();
     }
--- src/test/java/de/igorakkerman/kata/minumums/MinimumsTest.java
+++ src/test/java/de/igorakkerman/kata/minumums/MinimumsTest.java
@@ -10,10 +10,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class MinimumsTest {
 
     @Test
-    void negativeCount_IllegalArgumentThrown() {
+    void emptyList_negativeCount_IllegalArgumentThrown() {
         assertThatThrownBy(() -> minimums(emptyList(), -1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("count == -1 < 0");
+    }
+
+    @Test
+    void emptyList_countHigherThanInputSize_IllegalArgumentThrown() {
+        assertThatThrownBy(() -> minimums(emptyList(), 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("count == 1 > 0 == input.size()");
     }
 
     @Test
@@ -23,20 +30,9 @@ class MinimumsTest {
     }
 
     @Test
-    void emptyInput_1Count_emptyMinimums() {
-        assertThat(minimums(emptyList(), 1))
-                .isEmpty();
-    }
-
-    @Test
-    void emptyInput_2Count_emptyMinimums() {
-        assertThat(minimums(emptyList(), 2))
-                .isEmpty();
-    }
-
-    @Test
     void singletonInput_0Count_emptyMinimums() {
         assertThat(minimums(emptyList(), 0))
                 .isEmpty();
     }
+
 }
