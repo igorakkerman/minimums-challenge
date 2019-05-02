ac5d2a21f00f7de7da953bfd8f14b79d9f8429d7
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-05-02 14:28:00 +0200

    empty minimums

--- src/main/java/de/igorakkerman/kata/minumums/Minimums.java
+++ src/main/java/de/igorakkerman/kata/minumums/Minimums.java
@@ -1,12 +1,15 @@
 package de.igorakkerman.kata.minumums;
 
+import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+
 public class Minimums {
-    public static Object minimums(List<Object> items, int count) {
+    public static List<Integer> minimums(List<Integer> items, int count) {
         if (count < 0)
             throw new IllegalArgumentException("count == " + count + " < 0");
 
-        return null;
+        return emptyList();
     }
 }
--- src/test/java/de/igorakkerman/kata/minumums/MinimumsTest.java
+++ src/test/java/de/igorakkerman/kata/minumums/MinimumsTest.java
@@ -2,16 +2,41 @@ package de.igorakkerman.kata.minumums;
 
 import org.junit.jupiter.api.Test;
 
+import static de.igorakkerman.kata.minumums.Minimums.minimums;
 import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MinimumsTest {
 
     @Test
-    @SuppressWarnings("ResultOfMethodCallIgnored")
     void negativeCount_IllegalArgumentThrown() {
-        assertThatThrownBy(() -> Minimums.minimums(emptyList(), -1))
+        assertThatThrownBy(() -> minimums(emptyList(), -1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("count == -1 < 0");
+    }
+
+    @Test
+    void emptyInput_0Count_emptyMinimums() {
+        assertThat(minimums(emptyList(), 0))
+                .isEmpty();
+    }
+
+    @Test
+    void emptyInput_1Count_emptyMinimums() {
+        assertThat(minimums(emptyList(), 1))
+                .isEmpty();
+    }
+
+    @Test
+    void emptyInput_2Count_emptyMinimums() {
+        assertThat(minimums(emptyList(), 2))
+                .isEmpty();
+    }
+
+    @Test
+    void singletonInput_0Count_emptyMinimums() {
+        assertThat(minimums(emptyList(), 0))
+                .isEmpty();
     }
 }
